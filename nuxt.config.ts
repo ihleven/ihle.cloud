510e53011655bdd7edb2f1e2ed0bbc951e2ce65b
ihleven <github@ihleven.de>
2022-08-07 15:21:52 +0200

    pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: [
+        '@kevinmarrec/nuxt-pwa'
+      ],
+      pwa: {
+        workbox: {
+          enabled: false
+        }
+      }
 })
